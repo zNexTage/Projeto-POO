2d9ae1589f00534cca747df164a1638622c78a8b
Christian de Oliveira <christianoliveira1908@hotmail.com>
2019-11-19 21:00:17 -0300

    Pequenos ajustes no diagrama e comentarios
    Fiz pequenos ajustes nos diagramas e coloquei comentarios na classe principal.

--- Fonte/src/com/br/projetocadastrodecliente/Main.java
+++ Fonte/src/com/br/projetocadastrodecliente/Main.java
@@ -1,5 +1,10 @@
+/*Autor: Christian de Oliveira Bueno
+ * Data Inicio: 19/11/2019
+ * Data Fim:
+ * */
 package com.br.projetocadastrodecliente;
 
+//Classe Principal
 public class Main {
 
 	public static void main(String[] args) {
